0cf416f3ffb8c53385ccbede98adb390aaceaf23
Pedro Mendes <mendes@copasi.org>
2024-04-25 23:26:40 -0400

    writes a log file

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# create a log file
+log="$(date +"%FT%H%M%S").log"
+touch $log
+
 # runs all tests
 
 for d in ./????_*/ ; do 
   cd "$d"
-  ./run.sh
+  output=$(./run.sh)
+  printf '%s\n' "${output}"
+  printf '%s\n' "${output}" >> ../$log
   cd .. 
 done
 
