7fa1996641d9fcf6579ce87942aa974508b8bed7
Pedro Mendes <mendes@copasi.org>
2024-04-29 22:28:48 -0400

    now reports kinetic functions used in the model

--- tests/model_report.py
+++ tests/model_report.py
@@ -62,7 +62,7 @@ if( comps is None):
 else:
     ncomps = comps.shape[0]
     outf.write(f"==== Compartments: {ncomps} ====\n")
-    print(comps.to_string(columns=['type','initial_size', 'unit', 'dimensionality', 'expression', 'initial_expression'], header=['type','size_0', 'unit','D','expression','expression_0'], justify='left'), file=outf)
+    print(comps.to_string(columns=['type','initial_size', 'unit', 'dimensionality', 'expression', 'initial_expression'], header=['type','size_0', 'unit','D','expression','expression_0'], index_names=False), file=outf)
     outf.write("\n\n")
 
 # species
@@ -72,7 +72,7 @@ if( mspecs is None):
 else:
     nspecs = mspecs.shape[0]
     outf.write(f"==== Species: {nspecs} ====\n")
-    print(mspecs.to_string(columns=['type','initial_concentration', 'unit', 'compartment', 'expression', 'initial_expression'], header=['type','[]_0', 'unit','comp.', 'expression','expression_0'],justify='left'),  file=outf)
+    print(mspecs.to_string(columns=['type','initial_concentration', 'unit', 'compartment', 'expression', 'initial_expression'], header=['type','[]_0', 'unit','comp.', 'expression','expression_0'],index_names=False),  file=outf)
     outf.write("\n\n")
 
 # reactions
@@ -82,10 +82,21 @@ if( mreacts is None):
 else:
     nreacts = mreacts.shape[0]
     outf.write(f"==== Reactions: {nreacts} ====\n")
-    print(mreacts.to_string(columns=['scheme','function', 'mapping'], header=['scheme','function', 'mapping'], justify='left'), file=outf)
+    print(mreacts.to_string(columns=['scheme','function', 'mapping'], header=['scheme','function', 'mapping'], index_names=False), file=outf)
     outf.write("\n\n")
 
-# TODO: report on functions used
+    # report kinetic functions used
+    mfuncs = mreacts['function'].unique()
+    if( mfuncs is None):
+        outf.write(f"==== Kinetic Functions: 0 ====\n\n")
+    else:
+        nfuncs = mfuncs.shape[0]
+        outf.write(f"==== Kinetic Functions: {nfuncs} ====\n")
+        funcs = get_functions()
+        fdf = funcs[funcs.index.isin(mfuncs)]
+        print(fdf.to_string(columns=['formula'], header=False, index_names=False), file=outf)
+        outf.write("\n\n")
+
 
 # global quantities
 mparams = get_parameters()
@@ -94,7 +105,7 @@ if( mparams is None):
 else:
     nparams = mparams.shape[0]
     outf.write(f"==== Global Quantities: {nparams} ====\n")
-    print(mparams.to_string(columns=['type','initial_value','unit', 'expression','initial_expression'], header=['type','value_0','unit','expression', 'expression_0'], justify='left'), file=outf)
+    print(mparams.to_string(columns=['type','initial_value','unit', 'expression','initial_expression'], header=['type','value_0','unit','expression', 'expression_0'], index_names=False), file=outf)
     outf.write("\n\n")
 
 # events
@@ -104,7 +115,7 @@ if( mevents is None):
 else:
     nevents = mevents.shape[0]
     outf.write(f"==== Events: {nevents} ====\n")
-    print(mevents.to_string(columns=['trigger','delay','assignments','fire_at_initial_time', 'persistent','priority'], header=['trigger','delay','targets','fire_t0', 'persist', 'pri'], justify='left'), file=outf)
+    print(mevents.to_string(columns=['trigger','delay','assignments','fire_at_initial_time', 'persistent','priority'], header=['trigger','delay','targets','fire_t0', 'persist', 'pri'], index_names=False), file=outf)
     outf.write("\n\n")
 
 # TODO: should we create a report about the tasks? in which case we need to know if copasi or SBML loaded
