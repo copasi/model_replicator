65f3e1e2ca5f71758f7ed2919f7765324c0f02b4
Pedro Mendes <mendes@copasi.org>
2024-05-03 23:21:59 -0400

    first commit

---
+++ tests/0003_TwinsMedium/README.md
@@ -0,0 +1,3 @@
+# 0003_TwinsMedium
+
+creates a set of two exact replicate units with a simple binding reaction each, both connected to a medium unit.
---
+++ tests/0003_TwinsMedium/run.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# work out our folder name
+test=${PWD##*/}          # to assign to a variable
+test=${test:-/}          # to correct for the case where PWD=/
+
+# run sbmodelr
+../../sbmodelr -n ../sources/twins.gv -t c --add-medium ../sources/BindingKa.cps 2 > output
+
+
+# compare output and target
+difference=$(diff output target_stdout)
+if [[ $difference ]]; then
+  printf 'FAIL %s\n' "${test}"
+  exit
+fi
+
+# create model summary
+../model_report.py BindingKa_2.cps
+
+fail=0
+
+# check that medium exists
+if ! grep -Pq "medium\s+fixed" BindingKa_2.summary.txt; then
+  printf 'FAIL %s\n' "${test}"
+  fail=1
+fi
+
+# check that transport reactions to the medium exist
+n=$(grep -Pc "t_c_[12]-medium\s+c_[12] = c_medium\s+Mass action \(reversible\)" BindingKa_2.summary.txt)
+if ((n != 2))  ; then
+  printf 'FAIL %s\n' "${test}"
+  fail=1
+fi
+
+#check that all units were created
+n=$(grep -Pc "^[abc]_[12]\s+reactions" BindingKa_2.summary.txt)
+if ((n != 6))  ; then
+  printf 'FAIL %s\n' "${test}"
+  fail=1
+fi
+
+
+# check that the transport between two units exists
+if ! grep -Pq "t_c_1-2\s+c_1 = c_2\s+Mass action \(reversible\)" BindingKa_2.summary.txt; then
+  printf 'FAIL %s\n' "${test}"
+  fail=1
+fi
+
+if [ "$fail" = 0 ] ; then
+  printf 'PASS %s\n' "${test}"
+  rm BindingKa_2.summary.txt output *.cps
+fi
+
+
