adf7c1d1beb98b8ce0f2811e330e30e68e130876
Pedro Mendes <mendes@copasi.org>
2024-05-04 23:20:02 -0400

    first commit

---
+++ tests/0005_Uniform100/README.md
@@ -0,0 +1,3 @@
+# 0004_Random100
+
+creates a set of 100 units with a simple binding reaction each, where Ka has gaussian noise with 20% std dev.
---
+++ tests/0005_Uniform100/run.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# work out our folder name
+test=${PWD##*/}          # to assign to a variable
+test=${test:-/}          # to correct for the case where PWD=/
+
+# run sbmodelr
+../../sbmodelr  --pn Ka 0.2 uni ../sources/BindingKa.cps 100 > output
+
+# compare output and target
+difference=$(diff output target_stdout)
+if [[ $difference ]]; then
+  printf 'FAIL %s\n' "${test}"
+#  exit
+fi
+
+# create model summary
+../model_report.py BindingKa_100.cps
+
+fail=0
+
+# check that 100 Ka parameters exist
+n=$(grep -Pc "Ka_\d+\s+fixed" BindingKa_100.summary.txt)
+if ((n != 100))  ; then
+  printf 'FAIL %s\n' "${test}"
+  fail=1
+fi
+
+# get values of Ka
+grep -Po "Ka_\d+\s+fixed\s+(\d+\.\d+)" BindingKa_100.summary.txt | awk '{ print $3 }' > Ka.csv
+
+# test that the minimum is above 1000-1000*0.2 = 800
+m=$(awk 'NR == 1 || $1 < min {line = $0; min = $1}END{print line}' Ka.csv)
+#printf 'MIN %s\n' "$m"
+if (( $(echo "$m < 800" |bc -l) )); then
+  printf 'FAIL %s\n' "${test}"
+  fail=1
+fi
+
+# test that the maximum is below 1000+1000*0.2 = 1200
+m=$(awk 'NR == 1 || $1 > max {line = $0; max = $1}END{print line}' Ka.csv)
+#printf 'MAX %s\n' "$m"
+if (( $(echo "$m > 1200" |bc -l) )); then
+  printf 'FAIL %s\n' "${test}"
+  fail=1
+fi
+
+# this would calculate mean and stdved using only grep and awk !
+#grep -Po "Ka_\d+\s+fixed\s+(\d+\.\d+)" BindingKa_1000.summary.txt | awk '{ sum+=$3; sumsq +=$3^2 } END { print sum/NR, NR*(sqrt((sumsq-sum^2/NR)/NR))/sum }'
+
+
+if [ "$fail" = 0 ] ; then
+  printf 'PASS %s\n' "${test}"
+  rm BindingKa_100.summary.txt Ka.csv output *.cps
+fi
