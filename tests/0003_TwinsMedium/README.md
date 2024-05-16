9043b56960b1eabe5bdf6d72d430a19740b58972
Pedro Mendes <mendes@copasi.org>
2024-05-15 20:45:10 -0400

    adds check for compartments

--- tests/0003_TwinsMedium/README.md
+++ tests/0003_TwinsMedium/README.md
@@ -8,5 +8,6 @@ creates a set of two exact replicate units with a simple binding reaction each, 
 - gv file reads well
 - produces an expected warning about parameter scan
 - produces no other warning or error
+- two compartment replicates are created
 - medium unit is created
 - transport reactions are created
--- tests/0003_TwinsMedium/run.sh
+++ tests/0003_TwinsMedium/run.sh
@@ -23,31 +23,37 @@ if ! [[ $? = 0 ]]; then
   exit -1
 fi
 
+# check that all compartments were created
+n=$(grep -Pc "^compartment_[12]\s+fixed" BindingKa_2.summary.txt)
+if ((n != 2))  ; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 2"
+fi
+
 # check that medium exists
 if ! grep -Pq "medium\s+fixed" BindingKa_2.summary.txt; then
   printf 'FAIL %s\n' "${test}"
-  let "fail = $fail + 2"
+  let "fail = $fail + 4"
 fi
 
 # check that transport reactions to the medium exist
 n=$(grep -Pc "t_c_[12]-medium\s+c_[12] = c_medium\s+Mass action \(reversible\)" BindingKa_2.summary.txt)
 if ((n != 2))  ; then
   printf 'FAIL %s\n' "${test}"
-  let "fail = $fail + 4"
+  let "fail = $fail + 8"
 fi
 
 #check that all units were created
 n=$(grep -Pc "^[abc]_[12]\s+reactions" BindingKa_2.summary.txt)
 if ((n != 6))  ; then
   printf 'FAIL %s\n' "${test}"
-  let "fail = $fail + 8"
+  let "fail = $fail + 16"
 fi
-
 
 # check that the transport between two units exists
 if ! grep -Pq "t_c_1-2\s+c_1 = c_2\s+Mass action \(reversible\)" BindingKa_2.summary.txt; then
   printf 'FAIL %s\n' "${test}"
-  let "fail = $fail + 16"
+  let "fail = $fail + 32"
 fi
 
 if [ "$fail" = 0 ] ; then
