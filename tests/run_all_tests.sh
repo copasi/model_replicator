ff0a98aac8d3cdd2f82c9b04224c06b67873e3e8
Pedro Mendes <mendes@copasi.org>
2024-05-15 20:01:32 -0400

    returns the number of tests failed

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -3,14 +3,19 @@
 # create a log file
 log="$(date +"%FT%H%M%S").log"
 touch $log
+fail=0
 
 # runs all tests
+# returns the number of tests that failed
 
 for d in ./????_*/ ; do 
   cd "$d"
   output=$(./run.sh)
+  if ! [[ $? = 0 ]]; then
+    let "fail = $fail + 1"
+  fi
   printf '%s\n' "${output}"
   printf '%s\n' "${output}" >> ../$log
   cd .. 
 done
-
+exit $fail
