eabd4062346dff5477d691357aadbda4932aab8a
Pedro Mendes <mendes@copasi.org>
2024-05-17 11:32:31 -0400

    changed to 6x6 and removed normality test

--- tests/0007_CoupledArray/README.md
+++ tests/0007_CoupledArray/README.md
@@ -1,10 +1,10 @@
 # 0007_CoupledArray
 
-creates an array of 8x8 units of the Selkov-Wolf-Heinrich model coupled through transport of Y between cells (no external compartment)
+creates an array of 6x6 units of the Selkov-Wolf-Heinrich model coupled through transport of Y between cells (no external compartment)
 
 ## tests
 
-- 2D, 8x8 units, transport, norm noise on initial conditions
+- 2D, 6x6 units, transport, norm noise on initial conditions
 - cps file reads well
 - produces no warning or error
 - checks -o option creates correct filename
--- tests/0007_CoupledArray/run.sh
+++ tests/0007_CoupledArray/run.sh
@@ -5,7 +5,7 @@ test=${PWD##*/}          # to assign to a variable
 test=${test:-/}          # to correct for the case where PWD=/
 
 # run sbmodelr
-../../sbmodelr  -t Y -k 0.1 --pn X 0.1 norm --pn Y 0.1 norm  -o Wolf8x8Y.cps ../sources/Selkov-Wolf-Heinrich.cps 8 8 > output
+../../sbmodelr  -t Y -k 0.1 --pn X 0.1 norm --pn Y 0.1 norm  -o Wolf6x6Y.cps ../sources/Selkov-Wolf-Heinrich.cps 6 6 > output
 
 fail=0
 
@@ -17,23 +17,23 @@ if [[ $difference ]]; then
 fi
 
 # test -o option by checking if output file was created
-if ! [ -f Wolf8x8Y.cps ]; then
+if ! [ -f Wolf6x6Y.cps ]; then
   printf 'FAIL %s\n' "${test}"
   let "fail = $fail + 2"
 fi
 
 # create model summary
-../model_report.py Wolf8x8Y.cps >/dev/null
+../model_report.py Wolf6x6Y.cps >/dev/null
 if ! [[ $? = 0 ]]; then
   printf 'FAIL %s\n' "${test}"
   exit -1
 fi
 
 # get values of X_i_j
-grep -Po "X_\d+,\d+\s+reactions\s+(\d+\.\d+)\s" Wolf8x8Y.summary.txt | awk '{ print $3 }' > X.csv
+grep -Po "X_\d+,\d+\s+reactions\s+(\d+\.\d+)\s" Wolf6x6Y.summary.txt | awk '{ print $3 }' > X.csv
 
-# test if they look normal -- removed because 16 samples is too small for this test
-#../shapiro-wilk.py X.csv
+# test if they look normal --  removed because it fails too often
+#../normality_test.py X.csv
 #if [ "$?" = 1 ] ; then
 #  printf 'FAIL %s\n' "${test}"
 #  let "fail = $fail + 4"
@@ -46,18 +46,18 @@ if [ "$?" = 1 ] ; then
   let "fail = $fail + 8"
 fi
 
-# test if the stdev is 0.1
-../chisqtest-sd.py X.csv 0.1
+# test if the stdev is 0.1*mean
+../chisqtest-sd.py X.csv 0.491
 if [ "$?" = 1 ] ; then
   printf 'FAIL %s\n' "${test}"
   let "fail = $fail + 16"
 fi
 
 # get values of Y_i_j
-grep -Po "Y_\d+,\d+\s+reactions\s+(\d+\.\d+)\s" Wolf8x8Y.summary.txt | awk '{ print $3 }' > Y.csv
+grep -Po "Y_\d+,\d+\s+reactions\s+(\d+\.\d+)\s" Wolf6x6Y.summary.txt | awk '{ print $3 }' > Y.csv
 
-# test if they look normal -- removed because 16 samples is too small for this test
-#../shapiro-wilk.py Y.csv
+# test if they look normal -- removed because it fails too often
+#../normality_test.py Y.csv
 #if [ "$?" = 1 ] ; then
 #  printf 'FAIL %s\n' "${test}"
 #  let "fail = $fail + 32"
@@ -70,8 +70,8 @@ if [ "$?" = 1 ] ; then
   let "fail = $fail + 64"
 fi
 
-# test if the stdev is 0.1
-../chisqtest-sd.py Y.csv 0.1
+# test if the stdev is 0.1*mean
+../chisqtest-sd.py Y.csv 0.077
 if [ "$?" = 1 ] ; then
   printf 'FAIL %s\n' "${test}"
   let "fail = $fail + 128"
@@ -79,7 +79,7 @@ fi
 
 if [ "$fail" = 0 ] ; then
   printf 'PASS %s\n' "${test}"
-  rm Wolf8x8Y.summary.txt Wolf8x8Y.cps *.csv output
+  rm Wolf6x6Y.summary.txt Wolf6x6Y.cps *.csv output
 fi
 
 exit $fail
