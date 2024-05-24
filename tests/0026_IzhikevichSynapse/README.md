cb6b2b1c050ed85ce085d19fa29b379c0c7ef8d6
Pedro Mendes <mendes@copasi.org>
2024-05-24 15:17:22 -0400

    started creating test 00226

---
+++ tests/0026_IzhikevichSynapse/README.md
@@ -0,0 +1,15 @@
+# 0026_IzhikevichSynapse
+
+three replicates of IzhikevichBurstingNeuron.cps connected by chemical synapses by ODE *v* using a graphviz file
+
+## tests
+
+- 1D, 3 units, network, synaptic connection global quantity ODE
+- cps file reads well
+- network file reads well
+- output file named correctly
+- check that variable-dependent event is processed correctly
+- check that time dependent event is processed correctly
+- checks that there are three ODEs for v
+- checks that there are two ODEs for br_i_j (the post-synaptic bound receptor)
+- running the model through COPASI produces output
---
+++ tests/0026_IzhikevichSynapse/run.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# work out our folder name
+test=${PWD##*/}          # to assign to a variable
+test=${test:-/}          # to correct for the case where PWD=/
+
+fail=0
+
+# run sbmodelr
+../../sbmodelr -s v -n ../sources/1to2to3.gv -o Izhi_1-2-3.cps ../sources/IzhikevichBurstingNeuron.cps 3 > output
+
+# compare output and target
+difference=$(diff output target_stdout)
+if [[ $difference ]]; then
+  printf 'FAIL %s\n' "${test}"
+  fail=1
+fi
+
+exit $fail
+
+# create model summary
+../model_report.py FitzHugh-Nagumo_2.cps >/dev/null
+if ! [[ $? = 0 ]]; then
+  printf 'FAIL %s\n' "${test}"
+  exit -1
+fi
+
+# check that there is exactly one event
+n=$(grep -Pc "event\s+Time > 2 && false" FitzHugh-Nagumo_2.summary.txt)
+if ((n != 1))  ; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 2"
+fi
+
+# check that there are two ODEs for x
+n=$(grep -Pc "x_([12])\s+ode.*Values\[c_\1\] \* \( Values\[y_\1\] \+ Values\[x_\1\] - Values\[x_\1\] \^ 3 \/ 3 \+ Values\[z_\1\] \)" FitzHugh-Nagumo_2.summary.txt)
+if ((n != 2))  ; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 4"
+fi
+
+# failures in the synaptic constants all generate error code 8
+# check that the specific conductivity constant has been defined with the default value
+if ! grep -Pq "^g_c_x_1\,2_synapse\s+fixed\s+1\.0" FitzHugh-Nagumo_2.summary.txt; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 8"
+fi
+
+# check that the tau_d constant has been defined with the default value
+if ! grep -Pq "^tau_d_x_synapse\s+fixed\s+10\.0" FitzHugh-Nagumo_2.summary.txt; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 8"
+fi
+
+# check that the tau_r constant has been defined with the default value
+if ! grep -Pq "^tau_r_x_synapse\s+fixed\s+0\.5" FitzHugh-Nagumo_2.summary.txt; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 8"
+fi
+
+# check that the V0 constant has been defined with the default value
+if ! grep -Pq "^V0_x_synapse\s+fixed\s+-20\.0" FitzHugh-Nagumo_2.summary.txt; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 8"
+fi
+
+# check that the Vsyn constant has been defined with the default value
+if ! grep -Pq "^Vsyn_x_synapse\s+fixed\s+20\.0" FitzHugh-Nagumo_2.summary.txt; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 8"
+fi
+
+# check that there is an ODE for br_1_2 (the post-synaptic bound receptor)
+n=$(grep -Pc "br_x_1\,2\s+ode.*\( 1 \/ Values\[tau_r_x_synapse\] - 1 \/ Values\[tau_d_x_synapse\] \) \* \( 1 - Values\[br_x_1\,2\] \) \/ \( 1 \+ exp \( Values\[V0_x_synapse\] - Values\[x_1\] \) \) - Values\[br_x_1\,2\] \/ Values\[tau_d_x_synapse\]" FitzHugh-Nagumo_2.summary.txt)
+if ((n != 1))  ; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 16"
+fi
+
+#check that ODE for x_2 has the synaptic current term
+if ! grep -Pq "^x_2\s+ode.*\+ Values\[g_c_x_1\,2_synapse\] \* Values\[br_x_1\,2] \* \( Values\[Vsyn_x_synapse\]" FitzHugh-Nagumo_2.summary.txt; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 32"
+fi
+
+if [ "$fail" = 0 ] ; then
+  printf 'PASS %s\n' "${test}"
+  rm FitzHugh-Nagumo_2.summary.txt output *.cps
+fi
+
+exit $fail
