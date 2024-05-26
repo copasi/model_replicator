b7cc8d0bebfaf3757dc5836a2a088460ce444244
Pedro Mendes <mendes@copasi.org>
2024-05-25 22:44:49 -0400

    test for element notes

---
+++ tests/0027_PulsedDrugNotes/README.md
@@ -0,0 +1,14 @@
+# 0027_PulsedDrugNotes
+
+creates a set of 2 units of the PulsedDrug model
+
+## tests
+
+- 1D, 2 units, events, notes
+- cps file reads well
+- produces no warning or error
+- tests -o option to name output file
+- checks that compartment notes were copied
+- checks that species notes were copied
+- checks that global quantity notes were copied
+- validates the model with CopasiSE
---
+++ tests/0027_PulsedDrugNotes/run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# work out our folder name
+test=${PWD##*/}          # to assign to a variable
+test=${test:-/}          # to correct for the case where PWD=/
+
+fail=0
+
+# run sbmodelr
+../../sbmodelr  -t drug --pn Drug_dose 0.5 uni -n ../sources/twins.gv -o PD2.cps ../sources/PulsedDrug.cps 2 > output
+
+# compare output and target
+difference=$(diff output target_stdout)
+if [[ $difference ]]; then
+  printf 'FAIL %s\n' "${test}"
+  fail=1
+fi
+
+# poor man's way of checking existence of notes: grepping the cps file...
+
+# check that there are two notes for the compartment
+n=$(grep -c "a location within which every species is well mixed" PD2.cps)
+if ((n != 2))  ; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 2"
+fi
+
+# check that there are two notes for the S species
+n=$(grep -c "fixed substrate" PD2.cps)
+if ((n != 2))  ; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 4"
+fi
+
+# check that there are two notes for the Drug_dose global quantity
+n=$(grep -c "<p>the <i>amount</i> of drug added each time</p></body>" PD2.cps)
+if ((n != 2))  ; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 8"
+fi
+
+# validate the model with CopasiSE
+../CopasiSE -c . --nologo --validate PD2.cps > /dev/null 2>&1
+if ! [[ $? = 0 ]]; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 16"
+fi
+
+if [ "$fail" = 0 ] ; then
+  printf 'PASS %s\n' "${test}"
+  rm  PD2.cps output > /dev/null 2>&1
+fi
+
+exit $fail
