930d6fff23aad9ed9d14e3051427727057709a42
Pedro Mendes <mendes@copasi.org>
2024-06-02 20:55:43 -0400

    tests various errors/warnings generated in link creation

---
+++ tests/0507_LinkErrors1/README.md
@@ -0,0 +1,14 @@
+# 0507_LinkErrors1
+
+tests errors related with creating links
+
+## tests
+
+- 1D 2D, network, grid, transport, synaptic
+- appropriate error for transport of species that does not exist
+- appropriate error for transport of species that is not dependent on reactions
+- appropriate warning for transport between same unit
+- appropriate warning for synaptic connection using undirected network
+- appropriate error for synaptic connection in grid
+- appropriate error for synaptic link on global quantity that is not an ODE
+- appropriate warning for synaptic connection between same unit
---
+++ tests/0507_LinkErrors1/run.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# work out our folder name
+test=${PWD##*/}          # to assign to a variable
+test=${test:-/}          # to correct for the case where PWD=/
+
+fail=0
+
+# test non-existent species
+../../sbmodelr -t J -n ../sources/twins.gv ../sources/PulsedDrug.cps 2 > output 2>&1
+
+# check that the correct warning is issued
+if ! grep -q "ERROR: Species J does not exist in the model" output; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = 1"
+fi
+
+# test transport on fixed species
+../../sbmodelr -t S -n ../sources/twins.gv ../sources/PulsedDrug.cps 2 > output 2>&1
+
+# check that the correct warning is issued
+if ! grep -q "ERROR: S is a species that does not depend on reactions, no transport reactions can be added" output; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 2"
+fi
+
+# test transport between same unit
+../../sbmodelr -t A -n ../sources/self.gv ../sources/PulsedDrug.cps 2 > output 2>&1
+
+# check that the correct warning is issued
+if ! grep -q " Warning: transport on the same unit not allowed, ignoring 2 -> 2" output; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 4"
+fi
+
+# test synaptic link in grid configuration
+../../sbmodelr -s v ../sources/IzhikevichBurstingNeuron.cps 2 2 > output 2>&1
+
+# check that the correct error is issued
+if ! grep -q "ERROR: 2D or 3D grids cannot have synaptic connections" output; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 8"
+fi
+
+# test synaptic link in undirected network
+../../sbmodelr -s v -n ../sources/twins.gv ../sources/IzhikevichBurstingNeuron.cps 2 > output 2>&1
+
+# check that the correct error is issued
+if ! grep -q " Warning: network was defined as undirected, but synapses will be added as directed connections" output; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 16"
+fi
+
+# test synaptic link on global quantity that is not an ODE
+../../sbmodelr -s i -n ../sources/1to2.gv ../sources/IzhikevichBurstingNeuron.cps 2 > output 2>&1
+
+# check that the correct error is issued
+if ! grep -q "ERROR: i is a global variable that is not an ODE" output; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 32"
+fi
+
+# test synaptic link to self
+../../sbmodelr -s v -n ../sources/self.gv ../sources/IzhikevichBurstingNeuron.cps 2 > output 2>&1
+
+# check that the correct error is issued
+if ! grep -q " Warning: diffusive or synaptic coupling onto the same unit not allowed, ignoring 2 -> 2" output; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 64"
+fi
+
+# test diffusive connection on species that is not an ODE
+../../sbmodelr -d A -n ../sources/twins.gv ../sources/PulsedDrug.cps 2 > output 2>&1
+
+# check that the correct warning is issued
+if ! grep -q "ERROR: A is a species but it is not of type ODE" output; then
+  printf 'FAIL %s\n' "${test}"
+  let "fail = $fail + 128"
+fi
+
+if [ "$fail" = 0 ] ; then
+  printf 'PASS %s\n' "${test}"
+  rm output *.cps > /dev/null 2>&1
+fi
+
+exit $fail
