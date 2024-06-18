98df72cf886e56b2c4e06e21fcf84bea295be113
Pedro Mendes <mendes@copasi.org>
2024-06-18 12:49:26 -0400

    files for case 2

---
+++ examples/Cells_in_medium/ex1case2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# example 1, case 2
+
+# create 2 cells in a medium, with transport on Y, with a transport rate
+# constant of 3.2, random initial conditions where X and Y are within
+# +/- 1% of their value in the base unit, medium volume equal to 20 (such
+# that the fraction of cellular volume is 5/(20+5) = 0.2
+
+# total cell volume)
+
+../../sbmodelr \
+ --output case2.cps \
+ --add-medium \
+ --medium-volume 20 \
+ --transport Y \
+ --transport-k 1 \
+ --pn X 0.9 uni \
+ --pn Y 0.9 uni \
+ Selkov-Wolf-Heinrich_k2.5.cps \
+ 5
+
