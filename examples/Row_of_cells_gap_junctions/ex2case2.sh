f4042e448df3a887da3c13c68c193fdc8cfc222e
Pedro Mendes <mendes@copasi.org>
2024-07-03 20:03:33 -0400

    fixed vmax

--- examples/Row_of_cells_gap_junctions/ex2case2.sh
+++ examples/Row_of_cells_gap_junctions/ex2case2.sh
@@ -3,12 +3,14 @@
 # example 2, case 2
 
 # create 12 cells connected in a row (described in row_of_12.gv file)
-# with transport on IP3 through Michaelis Menten kinetics
-
+# with transport on IP3 through Hill kinetics
 
 ../../sbmodelr \
  --output ex2case2.cps \
- -m IP3 \
+ --Hill-transport IP3 \
+ --transport-Vmax 4 \
+ --transport-Km 0.72 \
+ --transport-h 4 \
  -n row_of_12.gv \
  ChI_DePitta2009.cps \
  12
