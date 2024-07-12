ed792acc572e3cceeb10ea7209d92fdc8dfb411f
Pedro Mendes <mendes@copasi.org>
2024-07-12 09:33:35 -0400

    ex3case1 results and plot
    Former-commit-id: 881f9ab3b2a7da9d873eb3bd92ae7ef14a5d5ca6

---
+++ examples/Array_of_oscillating_cells/ex3case1_plot.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+# load data
+data = np.genfromtxt(fname="ex3case1_data.tsv", delimiter="\t", skip_header=21, usecols=range(1,2402)).reshape((49,49))
+
+# figure dimensions
+png_width = 1000
+png_height = 800
+png_dpi = 100
+
+# plot the data
+fig, ax = plt.subplots(figsize=(png_width / png_dpi, png_height / png_dpi))
+im = ax.imshow(data, cmap=mpl.colormaps['jet'], origin='lower',  vmin=0, vmax=50)
+
+# add a colorbar
+cb = fig.colorbar(im, ax=ax,ticks=[0,10,20,30,40,50])
+
+# save
+plt.savefig("ex3case1.png", dpi=png_dpi)
