fe6d38938c77f3dd43a525f46ddbc6a17e0c5bb2
Pedro Mendes <mendes@copasi.org>
2024-07-18 15:31:44 -0400

    cas 1

---
+++ examples/Neuron_networks_I/ex4case1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# example 4, case 1
+
+# create a simple 2 neuron network where 1 feeds into 2
+
+
+../../sbmodelr \
+ --output ex4case1.cps \
+ -n ff2.dot \
+ --ode-synaptic V \
+ RSA_neuron.cps \
+ 2
