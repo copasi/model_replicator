5200fa603d1ef1b59a1280e470537fd5903fa1f7
Pedro Mendes <mendes@copasi.org>
2024-08-29 15:32:38 -0400

    ex4case4.sh

---
+++ examples/Neuron_networks_I/ex4case4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# example 4, case 4
+# spinal central pattern generator
+
+../../sbmodelr \
+ --output scpg.cps \
+ -n scpg.dot \
+ --ode-synaptic V \
+ --synapse-g 0.08 \
+ --cn 0 uni \
+ RSA_neuron.cps \
+ 6
