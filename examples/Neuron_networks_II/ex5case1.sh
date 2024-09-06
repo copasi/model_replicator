53d0ce3809dcce2d4c92701b97e2615c4ffc62af
Pedro Mendes <mendes@copasi.org>
2024-09-06 13:06:30 -0400

    README.md

---
+++ examples/Neuron_networks_II/ex5case1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# example 5, case 1
+
+# create network of 100 Izhikevic neurons connected in an Erdos topology with chemical synapses
+
+
+../../sbmodelr \
+ --output ex5case1.cps \
+ -n network100-200.dot \
+ --ode-synaptic v \
+ --synapse-g 0.2 \
+ --synapse-link-g \
+ Izhikevich.cps \
+ 100
